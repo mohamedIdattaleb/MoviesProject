eee1b6d616e26f720caadeea09f62a2180e4f938
Mohamedtwd <superraja2003@gmail.com>
2025-01-05 20:39:16 +0100

    ajout css

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css';
+
+
 function Login() {
     return (
-        <>
-            <div>
+        <div className='login-container'>
+            <div className='login'>
                 <h1>Movies Star</h1>
-                <h6>Login to your account</h6>
+                <h3>Login to your account</h3>
                 <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <Link to="/">Forgot Password</Link>
-                    <button>Login</button>
-            </form>
-            <span>Don't have an account?<Link to="/">Sign Up</Link></span>
+                    <label htmlFor="username">Username</label><br />
+                    <input 
+                        id="username" 
+                        type="text" 
+                        placeholder="Username" 
+                        required 
+                    /><br />
+
+                    <label htmlFor="password">Password</label><br />
+                    <input 
+                        id="password" 
+                        type="password" 
+                        placeholder="Password" 
+                        required 
+                    /><br />
+
+                    <Link to="/forgot-password" className="forgot-password-link">
+                        Forgot Password?
+                    </Link><br />
+
+                    <button type="submit">Login</button>
+                </form>
+                <span>
+                    Don't have an account? 
+                    <Link to="/signup" className="signup-link"> Sign Up</Link>
+                </span>
             </div>
-        </>
-    )
-};
+        </div>
+    );
+}
+
 export default Login;
